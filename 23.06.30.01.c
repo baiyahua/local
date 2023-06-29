f0a81b179496848addf9c77d0d0e9783547ccc96
tassel <examplename39@outlook.com>
2023-06-30 00:32:33 +0800

    23.06.30.01.c

---
+++ 23.06.30.01.c
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int my_strlen(char* arr)
+{
+	if (*arr != '\0')
+	{
+		return 1 + my_strlen(arr + 1);
+	}
+	else
+	{
+		return 0;
+	}
+}
+//void print(int n)
+//{
+//	if (n > 9)
+//	{
+//		print(n / 10);
+//	}
+//	printf("%d ", n % 10);
+//}
+
+int main(void)
+{
+	/*unsigned int n = 0;
+	scanf("%d", &n);
+	print(n);*/
+	char arr[] = "abc";
+	int m = my_strlen(arr);
+	printf("%d", m);
+
+	return 0;
+}
